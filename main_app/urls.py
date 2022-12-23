7931f440aa5cc42cc47dbbfa8b7d1d10977a858a
bbushraahmedd <bushimed98@gmail.com>
2022-12-22 21:44:29 -0600

    added cbvs

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('dragons/', views.dragons_index, name='index'),
     path('dragons/<int:dragon_id>/', views.dragons_detail, name='detail'),
+    path('dragons/create/', views.DragonCreate.as_view(), name='dragons_create'),
+    path('dragons/<int:pk>/update/', views.DragonUpdate.as_view(), name='dragons_update'),
+    path('dragons/<int:pk>/delete/', views.DragonDelete.as_view(), name='dragons_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dragon
 
 # Create your views here.
@@ -17,3 +18,15 @@ def dragons_index(request):
 def dragons_detail(request, dragon_id):
   dragon = Dragon.objects.get(id=dragon_id)
   return render(request, 'dragons/detail.html', {'dragon': dragon})
+
+class DragonCreate(CreateView):
+    model = Dragon
+    fields = '__all__'
+
+class DragonUpdate(UpdateView):
+    model = Dragon
+    fields = ['color', 'description']
+
+class DragonDelete(DeleteView):
+    model = Dragon
+    success_url = '/dragons/'
