98193b561cfe24e452995ed4928e96e118d1365c
bbushraahmedd <bushimed98@gmail.com>
2022-12-29 16:48:13 -0600

    updated

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Dragon
+from .models import Dragon, Feeding
 
 # Register your models here.
 
 admin.site.register(Dragon)
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 class Dragon(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
@@ -14,3 +20,17 @@ class Dragon(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dragon_id': self.id})
+
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    dragon = models.ForeignKey(Dragon, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
