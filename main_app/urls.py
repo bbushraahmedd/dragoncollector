aba44bfcc65f10dfbad0c4feb71963d4d2301c8d
bbushraahmedd <bushimed98@gmail.com>
2023-01-04 22:17:58 -0500

    M:M added

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('dragons/<int:pk>/update/', views.DragonUpdate.as_view(), name='dragons_update'),
     path('dragons/<int:pk>/delete/', views.DragonDelete.as_view(), name='dragons_delete'),
     path('dragons/<int:dragon_id>/add_feeding/', views.add_feeding, name='add_feeding'),
-]
+    path('dragons/<int:dragon_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+] 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dragon
+from django.views.generic import ListView, DetailView
+from .models import Dragon, Toy
 from .forms import FeedingForm
 
 # Create your views here.
@@ -18,10 +19,12 @@ def dragons_index(request):
 
 def dragons_detail(request, dragon_id):
   dragon = Dragon.objects.get(id=dragon_id)
+  toys_dragon_doesnt_have = Toy.objects.exclude(id__in = dragon.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'dragons/detail.html', {
     'dragon': dragon,
-    'feeding_form': feeding_form
+    'feeding_form': feeding_form,
+    'toys': toys_dragon_doesnt_have
     })
 
 def add_feeding(request, dragon_id):
@@ -43,3 +46,25 @@ class DragonUpdate(UpdateView):
 class DragonDelete(DeleteView):
     model = Dragon
     success_url = '/dragons/'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def assoc_toy(request, dragon_id, toy_id):
+  Dragon.objects.get(id=dragon_id).toys.add(toy_id)
+  return redirect('detail', dragon_id=dragon_id)
